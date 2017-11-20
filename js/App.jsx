52ff5abf701553ddba223c744a83b845bb91eeb5
jackyho112 <jackyho112dev@gmail.com>
2017-11-19 23:24:03 -0800

    Add App foundation files

---
+++ js/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import IssueViewer from './containers/IssueViewer';
+import './App.css';
+
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => (
+  <div className="app">
+    <Switch>
+      <Route exact path="/" component={props => <IssueViewer {...props} />} />
+      <Route component={FourOhFour} />
+    </Switch>
+  </div>
+);
+
+export default App;
